ce0fe807845d303503e8bc8e7e4a905e8868de6f
henriquez5 <henrique._jorge@hotmail.com>
2024-01-10 17:41:43 -0300

    Ajuste na classe de conexao com o BD

--- ProdutosWebAPI/Database/ConexaoDB.vb
+++ ProdutosWebAPI/Database/ConexaoDB.vb
@@ -8,25 +8,4 @@ Public Class ConexaoDB
         Return New SqlConnection(caminhoConexao)
     End Function
 
-    Public Sub Criar(produto As Produto, conexao As SqlConnection)
-
-        Dim query As String = "INSERT INTO Produtos (Nome, Categoria, Preco) OUTPUT INSERTED.Id VALUES (@Valor1, @Valor2, @Valor3)"
-
-        Using dataAdapter As New SqlDataAdapter()
-            Using commandSql As New SqlCommand(query, conexao)
-                commandSql.Parameters.AddWithValue("@Valor1", produto.Nome)
-                commandSql.Parameters.AddWithValue("@Valor2", produto.Categoria)
-                commandSql.Parameters.AddWithValue("@Valor3", produto.Preco)
-
-                dataAdapter.InsertCommand = commandSql
-
-                'commandSql.ExecuteNonQuery()
-
-                Dim novoId As Integer = CInt(commandSql.ExecuteScalar())
-
-                produto.Id = novoId
-
-            End Using
-        End Using
-    End Sub
 End Class
